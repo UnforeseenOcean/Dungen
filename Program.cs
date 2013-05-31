23401d73262423d6fed74c2e348684638988f3ba
Windsdon <jpfm2210@gmail.com>
2013-05-30 21:51:10 -0300

    Text coloring improved

--- Program.cs
+++ Program.cs
@@ -5,117 +5,59 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DungeonGenerator {
-
-    abstract class Item {
-        public ItemType itemID;
-        public long itemData;
-        public int stackSize;
-
-        public enum ItemType {
-            Key = 1024
-        }
-    }
-
-    abstract class Tile {
-        public TileType tileID;
-        public long tileData;
-
-        public enum TileType {
-            Air,
-            GenericFloor,
-            GenericWall,
-            Door,
-            Chest,
-            Spawn,
-            StairsUp,
-            StairsDown,
-            Undefined
-        }
-    }
-
-    class TileDoor : Tile {
-        private int doorNumber;
-
-        public TileDoor(int id, long data) {
-            tileID = (TileType)id;
-            tileData = data;
-
-            doorNumber = (int)data;
-        }
-
-        public TileDoor(int number) {
-            tileID = Tile.TileType.Door;
-            doorNumber = number;
-        }
-
-        public int getDoorNumber() {
-            return doorNumber;
-        }
-    }
-
-    abstract class Room {
-        abstract public Rectangle[] getCollisionBox();
-    }
-
-
-    /* 
-     * Basic rooms are rectangular, and always have one explicit entrance. Other rooms may attach to it.
-     * Their collision box is a rectangle of the same dimensions as the room.
-     */
-    class BasicRoom : Room {
-        private int x, y, w, h;
-
-        public BasicRoom(int x, int y, int w, int h) {
-            this.x = x;
-            this.y = y;
-            this.w = w;
-            this.h = h;
-        }
-
-        public Rectangle[] getCollisionBox() {
-            Rectangle[] r = new Rectangle[w * h];
-
-
-            for (int i = 0; i < r.Length; i++) {
-                r[i] = new Rectangle(x + i % w, y + i / w, 1, 1);
-            }
-
-            return r;
-        }
-    }
-
-    class TileChest : Tile {
-        public Item[] chestContents;
-
-        public TileChest(int id, long data) {
-            tileID = (TileType)id;
-            tileData = data;
-        }
-    }
-
-
-
-    class ItemKey : Item {
-        public ItemKey(int id, long data) {
-            itemID = (ItemType)id;
-            itemData = data;
-        }
-
-        public ItemKey(TileDoor door) {
-            itemData = door.getDoorNumber();
-        }
-    }
-
     /**
      * Holds all the dungeon information.
      */
     class Dungeon {
-        private Room[] rooms;
 
+        private enum Heading {
+            Info, Warning, Attention
+        }
+
+        private string[] headings = {
+                                        "§1Info§r",
+                                        "§2Warning§r",
+                                        "§8Attention§r"
+                                    };
+
+        private string getHeading(Heading h) {
+            return "§7[§r" + headings[(int)h] + "§7]§r";
+        }
+
+        private void print(Heading type, string text, object[] p) {
+            Program.printf(getHeading(type) + " " + text, p);
+            Console.Out.WriteLine();
+        }
+
+        public Dungeon(int width, int height, int floors, int chestCount, int roomsNumber, bool allowLockedDoors) {
+            print(Heading.Info, "Init new dungeon. Parameters:\n Width: {0}\n Height: {1}\n Floors: {2}\n Chests: {3}\n Rooms: {4}\n Locked doors: {5}\n",
+                new object[] {
+                    width, height, floors, chestCount, roomsNumber, allowLockedDoors ? "true" : "false"
+                });
+        }
+
+        /**
+         * The plan is: Generate roomsNumber rooms, outshift them and then connect with corridors, adding locked doors and making
+         * sure that the it's possible to reach every room.
+         * And I don't fukin' know how to do all of that.
+         * 
+         * Okay, let's start by creating the Dungeon itself. This object will contain all the map data, that is, the map itself as well as
+         * any object metadata (for monster spawners, mechanisms, chest contents, and so on.
+         * 
+         * Each object that has metadata will hava a unique ID, as the metadata for said object will be stored separetely, in JSON format.
+         * The map itself won't retain the Room information, and will be turned into a tileset, or so it will be for DunGen 1.0 file specification.
+         * 
+         * Also, this comments will be very helpful, as I'm most likely going to forget everything I just said, and will come back very
+         * often to remember what it is I have to implement next.
+         */
+
+        public void generate() {
+            print(Heading.Attention, "Starting dungeon generation...", null);
+        }
     }
 
     class Program {
-        private ConsoleColor[] CustomColors = new ConsoleColor[] { 
+        private static ConsoleColor[] CustomColors = new ConsoleColor[] { 
             ConsoleColor.Black, //0
             ConsoleColor.White, //1
             ConsoleColor.Red, //2
@@ -123,36 +65,20 @@ namespace DungeonGenerator {
             ConsoleColor.Blue, //4
             ConsoleColor.DarkBlue, //5
             ConsoleColor.DarkGreen, //6
-            ConsoleColor.Gray //7
+            ConsoleColor.Gray, //7
+            ConsoleColor.Yellow //8
         };
         private bool exitRequested;
 
-        /**
-         * This is the main method of the program, where it all happens.
-         */
-        private void generateDungeon(int width, int height, int floors, int chestCount, int roomsNumber, bool allowLockedDoors) {
-            /**
-             * The plan is: Generate roomsNumber rooms, outshift them and then connect with corridors, adding locked doors and making
-             * sure that the it's possible to reach every room.
-             * And I don't fukin' know how to do all of that.
-             **/
-
-            /**
-             * Okay, let's start by creating the Dungeon itself. This object will contain all the map data, that is, the map itself as well as
-             * any object metadata (for monster spawners, mechanisms, chest contents, and so on.
-             * 
-             * Each object that has metadata will hava a unique ID, as the metadata for said object will be stored separetely, in JSON format.
-             * The map itself won't retain the Room information, and will be turned into a tileset, or so it will be for DunGen 1.0 file specification.
-             * 
-             * Also, this comments will be very helpful, as I'm most likely going to forget everything I just said, and will come back very
-             * often to remember what it is I have to implement next.
-             */
-
-
+        public static void print(string str) {
+            printf(str, null);
         }
 
-        private void print(string str) {
+        public static void printf(string str, object[] p) {
             bool prevsim = false;
+            bool prevCurly = false;
+            bool curlyStart = false;
+            string parambuffer = "";
             ConsoleColor originalColor = Console.ForegroundColor;
             for (int i = 0; i < str.Length; i++) {
                 char c = str.ToCharArray(i, 1)[0];
@@ -171,9 +97,33 @@ namespace DungeonGenerator {
 
 
                     prevsim = false;
+                } else if (prevCurly) {
+                    if (!curlyStart) {
+                        parambuffer = "";
+                        curlyStart = true;
+                    }
+
+                    if (c.Equals('}')) {
+                        int k = int.Parse(parambuffer);
+                        try {
+                            print(p[k].ToString());
+                        } catch (Exception e) {
+                            continue;
+                        }
+
+                        curlyStart = false;
+                        prevCurly = false;
+                    } else if (c <= '9' && c >= '0') {
+                        parambuffer += c;
+                    }
+
+
                 } else {
                     if (c.Equals('§')) {
                         prevsim = true;
+                    } else if (c.Equals('{')) {
+                        prevCurly = true;
+                        curlyStart = false;
                     } else {
                         Console.Out.Write(c);
                     }
@@ -181,12 +131,12 @@ namespace DungeonGenerator {
             }
         }
 
-        private void println(string str) {
+        public static void println(string str) {
             print(str);
             Console.Out.WriteLine();
         }
 
-        private int getCharValue(char c) {
+        public static int getCharValue(char c) {
             if (c >= '0' && c <= '9') {
                 return c - '0';
             } else if (c >= 'a' && c <= 'f') {
@@ -223,15 +173,9 @@ namespace DungeonGenerator {
 
             if (pieces[1] == "demo") {
                 println("Generating a demo dungeon");
-                generateDungeon(100, 100, 1, 10, true);
+                new Dungeon(100, 100, 1, 10, 10, true).generate();
                 return;
             }
-        }
-
-
-
-        private bool intersects(Room a, Room b) {
-            Rectangle ra[] = a.getCollisionBox();
         }
 
         public int init() {
