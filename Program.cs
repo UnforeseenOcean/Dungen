06d687da8013b414382144080fd3327b37f27d87
Windsdon <jpfm2210@gmail.com>
2013-06-02 13:38:00 -0300

    Updated Stuffs

--- Program.cs
+++ Program.cs
@@ -5,56 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DungeonGenerator {
-    /**
-     * Holds all the dungeon information.
-     */
-    class Dungeon {
-
-        private enum Heading {
-            Info, Warning, Attention
-        }
-
-        private string[] headings = {
-                                        "§1Info§r",
-                                        "§2Warning§r",
-                                        "§8Attention§r"
-                                    };
-
-        private string getHeading(Heading h) {
-            return "§7[§r" + headings[(int)h] + "§7]§r";
-        }
-
-        private void print(Heading type, string text, object[] p) {
-            Program.printf(getHeading(type) + " " + text, p);
-            Console.Out.WriteLine();
-        }
-
-        public Dungeon(int width, int height, int floors, int chestCount, int roomsNumber, bool allowLockedDoors) {
-            print(Heading.Info, "Init new dungeon. Parameters:\n Width: {0}\n Height: {1}\n Floors: {2}\n Chests: {3}\n Rooms: {4}\n Locked doors: {5}\n",
-                new object[] {
-                    width, height, floors, chestCount, roomsNumber, allowLockedDoors ? "true" : "false"
-                });
-        }
-
-        /**
-         * The plan is: Generate roomsNumber rooms, outshift them and then connect with corridors, adding locked doors and making
-         * sure that the it's possible to reach every room.
-         * And I don't fukin' know how to do all of that.
-         * 
-         * Okay, let's start by creating the Dungeon itself. This object will contain all the map data, that is, the map itself as well as
-         * any object metadata (for monster spawners, mechanisms, chest contents, and so on.
-         * 
-         * Each object that has metadata will hava a unique ID, as the metadata for said object will be stored separetely, in JSON format.
-         * The map itself won't retain the Room information, and will be turned into a tileset, or so it will be for DunGen 1.0 file specification.
-         * 
-         * Also, this comments will be very helpful, as I'm most likely going to forget everything I just said, and will come back very
-         * often to remember what it is I have to implement next.
-         */
-
-        public void generate() {
-            print(Heading.Attention, "Starting dungeon generation...", null);
-        }
-    }
 
     class Program {
         private static ConsoleColor[] CustomColors = new ConsoleColor[] { 
@@ -167,13 +117,14 @@ namespace DungeonGenerator {
 
         private void generateDungeon(string[] pieces) {
             if (pieces.Length < 2) {
-                println("Uso: §3generate§r {§4width height§r | §4demo§r}");
+                println("Uso: §3generate§r (§4width height§r) | (§4demo§r)");
                 return;
             }
 
             if (pieces[1] == "demo") {
                 println("Generating a demo dungeon");
                 new Dungeon(100, 100, 1, 10, 10, true).generate();
+                //new DungeonDisplayer(null);
                 return;
             }
         }
@@ -181,23 +132,18 @@ namespace DungeonGenerator {
         public int init() {
             println("§6Init...§r");
             doRun();
+            //try {
+            //    dorun();
+            //} catch (exception e) {
+            //    printf("§2fatal exception§r: §8{0}§r\n {1}\n\n§2program terminated.", new string[] { e.message, e.stacktrace });
+            //    console.in.read();
+            //}
             return 0;
         }
 
         static int Main(string[] args) {
             Program a = new Program();
             return a.init();
-        }
-    }
-
-    struct Rectangle {
-        public int x, y, w, h;
-
-        public Rectangle(int x, int y, int w, int h) {
-            this.x = x;
-            this.y = y;
-            this.w = w;
-            this.h = h;
         }
     }
 }
